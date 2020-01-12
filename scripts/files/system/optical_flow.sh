803b89c17f17bf9989ce8c7f98fdf9f76bb055d7
REV <tech0xx@outlook.com>
2020-01-12 21:39:19 +0000

    Optical flow mode with range & without aruco

--- scripts/files/system/optical_flow.sh
+++ scripts/files/system/optical_flow.sh
@@ -1,3 +1,7 @@
-echo "Run optical flow"
+echo "Optical Flow Mode"
+rosnode kill aruco_detect
+rosnode kill optical_flow
+rosnode kill rangefinder
+/usr/bin/screen -Dm bash -c " source /home/pi/catkin_ws/devel/setup.bash && roslaunch clever_telegram range_vl53l1x.launch" &
 /usr/bin/screen -Dm bash -c " source /home/pi/catkin_ws/devel/setup.bash && roslaunch clever_telegram optical_flow.launch" &
 
