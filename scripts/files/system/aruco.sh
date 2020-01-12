92fedb5e31aa07df1207a17184f31dfd3a360b47
REV <tech0xx@outlook.com>
2020-01-12 21:36:29 +0000

    Aruco single mode, without range & optical flow

--- scripts/files/system/aruco.sh
+++ scripts/files/system/aruco.sh
@@ -1,4 +1,7 @@
-echo "Run aruco node"
+echo "Aruco Mode"
+rosnode kill aruco_detect
+rosnode kill optical_flow
+rosnode kill rangefinder
 /usr/bin/screen -Dm bash -c " source /home/pi/catkin_ws/devel/setup.bash && roslaunch clever aruco.launch" &
 
 
