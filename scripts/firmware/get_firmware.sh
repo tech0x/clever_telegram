5712ec668564e870abda15af5ac050822b150df0
REV <tech0xx@outlook.com>
2020-01-16 21:49:52 +0000

    Firmware upload from raspberry

---
+++ scripts/firmware/get_firmware.sh
@@ -0,0 +1,23 @@
+mkdir v1.8.2-clever.9
+cd v1.8.2-clever.9
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.8.2-clever.9/px4fmu-v2_default.px4
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.8.2-clever.9/px4fmu-v2_lpe.px4
+#wget https://github.com/CopterExpress/Firmware/releases/download/v1.8.2-clever.9/px4fmu-v3_default.px4
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.8.2-clever.9/px4fmu-v4_default.px4
+#wget https://github.com/CopterExpress/Firmware/releases/download/v1.8.2-clever.9/px4fmu-v5_default.px4
+
+mkdir v1.10.0-clever.1
+cd v1.10.0-clever.1
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.10.0-clever.1/px4_fmu-v2_default.px4
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.10.0-clever.1/px4_fmu-v2_lpe.px4
+#wget https://github.com/CopterExpress/Firmware/releases/download/v1.10.0-clever.1/px4_fmu-v3_default.px4
+wget https://github.com/CopterExpress/Firmware/releases/download/v1.10.0-clever.1/px4_fmu-v4_default.px4
+#wget https://github.com/CopterExpress/Firmware/releases/download/v1.10.0-clever.1/px4_fmu-v5_default.px4
+
+mkdir v1.10.0-px4
+cd v1.10.0-px4
+wget https://github.com/PX4/Firmware/releases/download/v1.10.0/px4_fmu-v2_default.px4
+wget https://github.com/PX4/Firmware/releases/download/v1.10.0/px4_fmu-v2_lpe.px4
+#wget https://github.com/PX4/Firmware/releases/download/v1.10.0/px4_fmu-v3_default.px4
+wget https://github.com/PX4/Firmware/releases/download/v1.10.0/px4_fmu-v4_default.px4
+#wget https://github.com/PX4/Firmware/releases/download/v1.10.0/px4_fmu-v5_default.px4
---
+++ scripts/firmware/upload.sh
@@ -0,0 +1,2 @@
+
+px_uploader.py --port /dev/serial/by-id/usb-3D_Robotics_PX4_BL_FMU_v4.x_0-if00 px4fmu-v4_default.px4
