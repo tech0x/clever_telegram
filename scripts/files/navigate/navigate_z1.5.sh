24d364337634ca377e486b1fcb78a938acb4c80b
REV <tech0xx@outlook.com>
2020-01-16 20:32:19 +0000

    Landing

--- scripts/files/navigate/navigate_z1.5.sh
+++ scripts/files/navigate/navigate_z1.5.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 rosservice call /navigate "{z: 1.5, frame_id: 'body', auto_arm: true}"
+sleep 5
+rosservice call /land "{}"
--- scripts/files/navigate/navigate_z1.sh
+++ scripts/files/navigate/navigate_z1.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-rosservice call /navigate "{z: 1.0, frame_id: 'body', auto_arm: true}"
+rosservice call /navigate "{x: 0.0, y: 0.0, z: 1.0, yaw: 1.0, yaw_rate: 0.0, speed: 0.1, frame_id: 'body', auto_arm: true}"
+sleep 5
+rosservice call /land "{}"
